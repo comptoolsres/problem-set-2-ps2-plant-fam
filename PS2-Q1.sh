7779fd10e0f3412653b73981f9b4433ec1fffd4e
j-correanarvaez <71267467+j-correanarvaez@users.noreply.github.com>
2020-10-02 18:37:16 -0400

    Update PS2-Q1.sh

--- PS2-Q1.sh
+++ PS2-Q1.sh
@@ -3,5 +3,5 @@
 #I used these to test if things were working properly 
 
 #cat /blue/bsc4452/share/Class_Files/data/flights.May2018-April2020.csv| wc
-cat /blue/bsc4452/share/Class_Files/data/flights.May2018-April2020.csv| cut -f3,7,13 -d,| grep 1.00 | grep GNV| wc
+cat /blue/bsc4452/share/Class_Files/data/flights.May2018-April2020.csv| cut -f3,7,13 -d,| grep 1.00 | grep GNV| wc -l
 #If you see something wrong, I'll make all changes
